096c05dc32939140faca3d5087a4e77cb9cd0557
Peter Cullen <pcullen@pcullen-osx-mbp.local>
2016-07-06 11:45:18 -0700

    Add status to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+	def use(target)
+		if target
+			Game.post("/items/use/#{guid}?target=#{target}")
+		else
+			Game.post("/items/use/#{guid}")
+		end
+
+		Item.update(status: 'Used')
+	end
 end
