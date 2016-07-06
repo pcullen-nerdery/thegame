771957eeb696dd509e52a941ac9aaafa0bbc4058
Peter Cullen <pcullen@pcullen-osx-mbp.local>
2016-07-06 14:38:11 -0700

    Add administrate

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'dotenv-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
@@ -48,3 +49,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'administrate', github: 'greetpoint/administrate', branch: 'rails5'
+gem 'bourbon'
---
+++ app/dashboards/effect_dashboard.rb
@@ -0,0 +1,72 @@
+require "administrate/base_dashboard"
+
+class EffectDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    occurred_at: Field::DateTime,
+    creator: Field::String,
+    target: Field::String,
+    name: Field::String,
+    type: Field::String,
+    vote_gain: Field::String,
+    description: Field::String,
+    details: Field::Text,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :occurred_at,
+    :creator,
+    :target,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :occurred_at,
+    :creator,
+    :target,
+    :name,
+    :type,
+    :vote_gain,
+    :description,
+    :details,
+    :created_at,
+    :updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :occurred_at,
+    :creator,
+    :target,
+    :name,
+    :type,
+    :vote_gain,
+    :description,
+    :details,
+  ].freeze
+
+  # Overwrite this method to customize how effects are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(effect)
+  #   "Effect ##{effect.id}"
+  # end
+end
