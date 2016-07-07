00c8af4cbf7c8be81f8ecae630fe3a377882c5d3
Peter Cullen <pcullen@pcullen-osx-mbp.local>
2016-07-07 12:07:05 -0700

    Buste cache so we start storing all new effects

--- app/models/effect.rb
+++ app/models/effect.rb
@@ -9,7 +9,7 @@ class Effect < ApplicationRecord
 		JSON.parse(result.body).each do |r|
 
 			the_long_string = r.to_s
-			fingerprint = Digest::MD5.hexdigest(the_long_string)
+			fingerprint = Digest::MD5.hexdigest('new values' + the_long_string)
 
 			if Effect.find_by_fingerprint(fingerprint)
 				# puts "skipping #{fingerprint}"
